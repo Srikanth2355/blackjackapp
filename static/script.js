c26429e6161d4ff0f7c134d267275533cea30cc8
Srikanth <gubbasrikanth2355@gmail.com>
2021-01-20 20:25:39 +0530

    challenge 2 cat generator completed

--- static/script.js
+++ static/script.js
@@ -8,5 +8,11 @@ function ageindays(){
 function reset() {
     document.getElementById('ageindays').remove();
     // document.getElementById('ageindays').innerText = null;
+}
 
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+    div.appendChild(image);
 }
